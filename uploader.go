a85fc70bb9b692533e30043a5c3ee8e64b81576d
Lassi Laiho <lassi.laiho@solita.fi>
2021-12-08 17:18:46 +0000

    Improve error messages

--- uploader.go
+++ uploader.go
@@ -149,58 +149,59 @@ func (u *fileUploader) createToken(bagName string) (string, error) {
 	return signedToken, err
 }
 
-func uploadURLErr(err error) error {
-	return fmt.Errorf("failed to request upload URL: %w", err)
+func wrapErr(format string, err *error, a ...interface{}) {
+	if *err != nil {
+		*err = fmt.Errorf(format, append([]interface{}{*err}, a...)...)
+	}
 }
 
-func (u *fileUploader) requestUploadURL(ctx context.Context, bagName, endpoint string) (string, error) {
+func (u *fileUploader) requestUploadURL(ctx context.Context, bagName, endpoint string) (_ string, err error) {
+	defer wrapErr("failed to request upload URL: %w", &err)
 	req, err := http.NewRequestWithContext(ctx, "POST", endpoint, nil)
 	if err != nil {
-		return "", uploadURLErr(err)
+		return "", fmt.Errorf("failed to create request: %w", err)
 	}
 	token, err := u.createToken(bagName)
 	if err != nil {
-		return "", uploadURLErr(err)
+		return "", fmt.Errorf("failed to create token: %w", err)
 	}
 	req.Header.Add("Authorization", "Bearer "+token)
 	resp, err := u.HTTPClient.Do(req)
 	if err != nil {
-		return "", uploadURLErr(err)
+		return "", fmt.Errorf("failed to send request: %w", err)
 	}
 	defer resp.Body.Close()
-	var respData struct {
-		URL, Error string
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %w", err)
 	}
-	if err := json.NewDecoder(resp.Body).Decode(&respData); err != nil {
-		io.Copy(io.Discard, resp.Body)
-		return "", uploadURLErr(err)
+	var respData struct{ URL, Error string }
+	if err := json.Unmarshal(body, &respData); err != nil {
+		return "", fmt.Errorf("response is invalid JSON: %w: %q", err, body)
 	}
 	if resp.StatusCode != 200 {
-		return "", uploadURLErr(errors.New(respData.Error))
+		return "", fmt.Errorf("request failed with code %d: %s", resp.StatusCode, respData.Error)
 	}
 	return respData.URL, nil
 }
 
-func uploadFileErr(err error) error {
-	return fmt.Errorf("failed to upload file: %w", err)
-}
-
-func (u *fileUploader) uploadFile(ctx context.Context, url string, file io.Reader) error {
+func (u *fileUploader) uploadFile(ctx context.Context, url string, file io.Reader) (err error) {
+	defer wrapErr("failed to upload file: %w", &err)
 	req, err := http.NewRequestWithContext(ctx, "PUT", url, file)
 	if err != nil {
-		return uploadFileErr(err)
+		return fmt.Errorf("failed to create request: %w", err)
 	}
 	resp, err := u.HTTPClient.Do(req)
 	if err != nil {
-		return uploadFileErr(err)
+		return fmt.Errorf("failed to send request: %w", err)
 	}
 	defer resp.Body.Close()
 	msg, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return uploadFileErr(err)
+		return fmt.Errorf("failed to read response: %w", err)
 	}
 	if resp.StatusCode != 200 {
-		return uploadFileErr(fmt.Errorf("HTTP error: code %d, %s", resp.StatusCode, msg))
+		return fmt.Errorf("HTTP error: code %d, %s", resp.StatusCode, msg)
 	}
 	return nil
 }
